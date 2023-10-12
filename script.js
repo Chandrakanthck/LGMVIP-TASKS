d794f30567ca8a7c3e3a203fcb6dbdfd7bb5090a
Chandrakanth <37268061+Chandrakanthck@users.noreply.github.com>
2023-10-12 21:07:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const inputBox = document.getElementById('input-box');
+const listcontainer = document.getElementById("list-container");
+
+function AddTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    savedata();
+}
+
+listcontainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        savedata();
+    }
+}, false);
+
+ function savedata(){
+    localStorage.setItem("data", listcontainer.innerHTML);
+ }
+    function loaddata(){
+        listcontainer.innerHTML = localStorage.getItem("data");
+    }
+    function showtask(){
+        listcontainer.innerHTML = localStorage.getItem("data");
+    }
+ showtask();
+ 
+ const darkModeToggle = document.getElementById('dark-mode-toggle');
+ const body = document.body;
+ 
+ darkModeToggle.addEventListener('click', () => {
+     body.classList.toggle('dark-mode'); // Add a class to the body to enable dark mode
+     saveDarkModeState(body.classList.contains('dark-mode')); // Save the dark mode state
+ });
+ 
+ function saveDarkModeState(isDarkMode) {
+     localStorage.setItem('dark-mode', isDarkMode);
+ }
+ 
+ function loadDarkModeState() {
+     const isDarkMode = localStorage.getItem('dark-mode');
+     if (isDarkMode === 'true') {
+         body.classList.add('dark-mode');
+     }
+ }
+ 
+ loadDarkModeState();
+ 
